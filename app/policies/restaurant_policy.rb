19d05ca20bc36ed6422a708e989d71e803a0c1d9
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-12-17 11:21:07 +0100

    finished exercise

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,26 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      # scope.where(user: user)
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
+  end
+end
